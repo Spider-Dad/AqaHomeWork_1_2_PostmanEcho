dd5cb88c7485e0610a9376cc87d548946dd91a6f
Pavel Shvydkin <Pshvydkin@gmail.com>
2023-06-18 21:03:03 +0300

    add PostmanEchoTest, build should passing

---
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -0,0 +1,33 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
+
+public class PostmanEchoTest {
+    @Test
+    public void testPostWithJsonBody (){
+        // Given - When - Then
+        // Предусловия
+         String jsonBody = "{\"id\": 1, \"name\": \"Petr\", \"number\": 1234, \"balance\": 100, \"currency\": \"RUB\"}";
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .header("Content-Type","application/json")
+                .body(jsonBody)
+                // Выполняемые действия
+                .when()
+                .post("/post")
+                // Проверки
+                .then()
+                .statusCode(200)
+                .body("json.id", equalTo(1))
+                .body("json.name", equalTo("Petr"))
+                .body("json.number",equalTo(1234))
+                .body("json.balance", greaterThanOrEqualTo(50))
+                .body("json.currency", equalTo("RUB"))
+        ;
+    }
+}
